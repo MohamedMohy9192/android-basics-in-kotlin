48f18c65e4fe238f8f869f64423011f5b9ed596e
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-09 20:10:13 +0200

    Move the letters list fromto newely created letter list fragment

--- Words App/app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ Words App/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -29,56 +29,12 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-
-    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        recyclerView = binding.recyclerView
-
-        // Sets the LinearLayoutManager of the recyclerview
-       chooseLayout()
     }
-
-    private fun chooseLayout() {
-        if (isLinearLayoutManager) {
-            recyclerView.layoutManager = LinearLayoutManager(this)
-        } else {
-            recyclerView.layoutManager = GridLayoutManager(this, 4)
-        }
-        recyclerView.adapter = LetterAdapter()
-    }
-
-    private fun setIcon(menuItem: MenuItem?) {
-        menuItem ?: return
-        menuItem.icon =
-            if (isLinearLayoutManager) ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
-            else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
-
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.layout_menu, menu)
-        val layoutButton = menu?.findItem(R.id.action_switch_layout)
-        setIcon(layoutButton)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_switch_layout -> {
-            // Sets isLinearLayoutManager (a Boolean) to the opposite value
-            isLinearLayoutManager = !isLinearLayoutManager
-            chooseLayout()
-            setIcon(item)
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
-    }
-
 }
 
