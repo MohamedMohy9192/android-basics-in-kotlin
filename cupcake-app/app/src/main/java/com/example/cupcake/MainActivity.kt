de37bb4c9acbf2f5b39ffceea2c97ba33a7dddec
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-13 03:22:18 +0200

    Setup navigation component with fragments transactions
    Cupcake App: Add Safe Args plugin dependency, create navigation actions between fragments, implement the navigation in code, and wire up the navigation component with the action bar.

--- cupcake-app/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ cupcake-app/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,24 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Get an instance of NavController from the NavHostFragment.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Passing in the instance of NavController. This will do the following:
+        // Show a title in the app bar based off of the destination's label,
+        // and display the Up button whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
+    }
+}
