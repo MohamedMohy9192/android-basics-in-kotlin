b475c541a01fdd5d7a9327a2b75d8af136211b1e
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-13 04:00:33 +0200

    Create an order view model with initial data properties

---
+++ cupcake-app/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+
+class OrderViewModel : ViewModel() {
+
+    private val _quantity: MutableLiveData<Int> = MutableLiveData(0)
+    val quantity: LiveData<Int> get() = _quantity
+
+    private val _flavor: MutableLiveData<String> = MutableLiveData("")
+    val flavor: LiveData<String> get() = _flavor
+
+    private val _pickupDate: MutableLiveData<String> = MutableLiveData("")
+    val pickupDate: LiveData<String> get() = _pickupDate
+
+    private val _price: MutableLiveData<Double> = MutableLiveData(0.0)
+    val price: LiveData<Double> get() = _price
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setData(pickupDate: String) {
+        _pickupDate.value = pickupDate
+    }
+}
