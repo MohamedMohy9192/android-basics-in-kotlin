0b4276c2c255ef5c0b12dc5ea3aa7a926f0c5a42
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-19 15:48:37 +0200

    Create the item DAO

--- inventory-app/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ inventory-app/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,4 +1,28 @@
 package com.example.inventory.data
 
-class ItemDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+
+    /**
+     * The OnConflictStrategy.IGNORE strategy ignores a new item if it's primary key
+     * is already in the database.
+     * */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * FROM item WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * FROM item ORDER BY name ASC")
+    fun getItems(): Flow<List<Item>>
+
 }
