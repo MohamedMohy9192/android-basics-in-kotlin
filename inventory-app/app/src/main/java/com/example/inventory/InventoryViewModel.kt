2f45425be203b22d96747e3ef30842218f761f10
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-03-19 17:36:05 +0200

    Add a InventoryViewModel

---
+++ inventory-app/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+}
+
+class InventoryViewModelFactory(
+    private val itemDao: ItemDao
+) : ViewModelProvider.Factory {
+    /**
+     * Takes any class type as an argument and returns a ViewModel object.
+     * */
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        //  Check if the modelClass is the same as the InventoryViewModel class and return
+        //  an instance of it. Otherwise, throw an exception.
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw  IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
